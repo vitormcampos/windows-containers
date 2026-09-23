86fbbf5aaf4c7185d1d8e4c84af7b728f24a62fb
Vitor Campos <vitormcampos99@gmail.com>
2026-09-23 19:00:02 -0300

    refactor(settings): align settings with supported wslc capabilities

--- WindowsContainers.UI/ViewModels/MainViewModel.cs
+++ WindowsContainers.UI/ViewModels/MainViewModel.cs
@@ -1,3 +1,4 @@
+using WindowsContainers.UI.Models;
 using WindowsContainers.UI.Services.Interfaces;
 
 namespace WindowsContainers.UI.ViewModels;
@@ -6,22 +7,29 @@ public class MainViewModel : ViewModelBase
 {
     private int _selectedNavigationIndex;
     private ViewModelBase _currentPage;
+    private bool _showStatusBar = true;
+    private bool _compactMode;
+    private double _sidebarWidth = 240;
+    private readonly IThemeService _themeService;
+    public IProviderService Provider { get; }
 
-    public MainViewModel(IEnvironmentState environment, SettingsViewModel settings, IProviderService provider)
+    public MainViewModel(IEnvironmentState environment, SettingsViewModel settings, IThemeService themeService, IProviderService provider)
     {
         Dashboard = new DashboardViewModel(environment);
         Images = new ImagesPageViewModel(provider);
         Settings = settings;
+        _themeService = themeService;
         Provider = provider;
         StatusBar = new StatusBarViewModel(environment);
         _currentPage = Dashboard;
+        ApplyAppearance(settings.CurrentSettings);
+        Settings.SettingsApplied += OnSettingsApplied;
     }
 
     public DashboardViewModel Dashboard { get; }
     public ImagesPageViewModel Images { get; }
     public SettingsViewModel Settings { get; }
     public StatusBarViewModel StatusBar { get; }
-    public IProviderService Provider { get; }
 
     public int SelectedNavigationIndex
     {
@@ -44,5 +52,33 @@ public class MainViewModel : ViewModelBase
     {
         get => _currentPage;
         private set => SetProperty(ref _currentPage, value);
+    }
+
+    public bool ShowStatusBar
+    {
+        get => _showStatusBar;
+        private set => SetProperty(ref _showStatusBar, value);
+    }
+
+    public bool CompactMode
+    {
+        get => _compactMode;
+        private set => SetProperty(ref _compactMode, value);
+    }
+
+    public double SidebarWidth
+    {
+        get => _sidebarWidth;
+        private set => SetProperty(ref _sidebarWidth, value);
+    }
+
+    private void OnSettingsApplied(object? sender, ApplicationSettings settings) => ApplyAppearance(settings);
+
+    private void ApplyAppearance(ApplicationSettings settings)
+    {
+        _themeService.Apply(settings.Theme);
+        ShowStatusBar = settings.ShowStatusBar;
+        CompactMode = settings.CompactMode;
+        SidebarWidth = CompactMode ? 200 : 240;
     }
 }
